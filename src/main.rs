d1631e79ee326edc2a869b70852cda1fe125f0ab
Francis Masha <francismasha96@gmail.com>
2023-04-08 14:39:01 +0300

    chore: add github workflows

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,9 @@ async fn main() -> anyhow::Result<()> {
 	let configuration = get_configuration().expect("Failed to read configuration.");
 	let application = Application::build(configuration.clone()).await?;
 	let application_task = tokio::spawn(application.run_until_stopped());
-	// let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
 
 	tokio::select! {
 		o = application_task => report_exit("API", o),
-		// o = worker_task =>  report_exit("Background worker", o),
 	};
 
 	Ok(())
