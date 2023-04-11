953bddccc0f6c04b8a9a928ff4badad583df9d90
Francis Masha <francismasha96@gmail.com>
2023-04-11 21:54:09 +0300

    feat: add update user endpoint

--- src/routes/handler.rs
+++ src/routes/handler.rs
@@ -1,7 +1,7 @@
 use crate::startup::AppState;
 use crate::users::model::UserModel;
-use crate::users::schema::{CreateUserSchema, FilterOptions};
-use actix_web::{get, post, web, HttpResponse, Responder};
+use crate::users::schema::{CreateUserSchema, FilterOptions, UpdateUserSchema};
+use actix_web::{get, patch, post, web, HttpResponse, Responder};
 use serde_json::json;
 
 #[get("/health_check")]
@@ -74,13 +74,9 @@ pub async fn create_user(
 #[get("/users/{id}")]
 pub async fn fetch_user(path: web::Path<uuid::Uuid>, data: web::Data<AppState>) -> impl Responder {
 	let user_id = path.into_inner();
-	let query_result = sqlx::query_as!(
-		UserModel,
-		"SELECT * FROM users WHERE id = $1",
-		user_id,
-	)
-	.fetch_one(&data.db)
-	.await;
+	let query_result = sqlx::query_as!(UserModel, "SELECT * FROM users WHERE id = $1", user_id,)
+		.fetch_one(&data.db)
+		.await;
 
 	match query_result {
 		Ok(user) => {
@@ -94,12 +90,63 @@ pub async fn fetch_user(path: web::Path<uuid::Uuid>, data: web::Data<AppState>) 
 	}
 }
 
+#[patch("/users/{id}")]
+pub async fn update_user(
+	path: web::Path<uuid::Uuid>,
+	body: web::Json<UpdateUserSchema>,
+	data: web::Data<AppState>,
+) -> impl Responder {
+	let user_id = path.into_inner();
+
+	let query_result = sqlx::query_as!(UserModel, "SELECT * FROM users WHERE id = $1", user_id,)
+		.fetch_one(&data.db)
+		.await;
+
+	if query_result.is_err() {
+		let message = format!("User with id {} not found", user_id);
+		let json_response = json!({ "status": "error", "message": message });
+		return HttpResponse::NotFound().json(json_response);
+	}
+
+	let user = query_result.unwrap();
+
+	let query_result = sqlx::query_as!(
+		UserModel,
+		"UPDATE users SET username = $1, email = $2, active = $3 WHERE id = $4 RETURNING *",
+		body.username.to_owned().unwrap_or(user.username),
+		body.email.to_owned().unwrap_or(user.email),
+		body.active,
+		user_id,
+	)
+	.fetch_one(&data.db)
+	.await;
+
+	match query_result {
+		Ok(user) => {
+			let json_response = json!({ "status": "success", "data": user });
+			HttpResponse::Ok().json(json_response)
+		}
+		Err(e) => {
+			if e.to_string()
+				.contains("duplicate key value violates unique constraint")
+			{
+				let json_response = json!({ "status": "error", "message": "User already exists" });
+				return HttpResponse::BadRequest().json(json_response);
+			}
+
+			let json_response = json!({ "status": "error", "message": e.to_string() });
+			HttpResponse::InternalServerError().json(json_response)
+		}
+	}
+}
+
 pub fn config(conf: &mut web::ServiceConfig) {
 	let scope = web::scope("/api")
 		.service(health_check)
 		.service(fetch_users)
 		.service(create_user)
-		.service(fetch_user);
+		.service(fetch_user)
+		.service(update_user);
 
 	conf.service(scope);
 }
